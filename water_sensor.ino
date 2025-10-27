ea05fd8685e017998260b3eebb7875405ac52d57
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-10-27 13:24:05 -0700

    Indent fix

--- water_sensor.ino
+++ water_sensor.ino
@@ -6,30 +6,30 @@
 int val = 0;
 
 void setup() {
-  // Set D7 as an OUTPUT
-  pinMode(sensorPower, OUTPUT);
+    // Set D7 as an OUTPUT
+    pinMode(sensorPower, OUTPUT);
 
-  // Set to LOW so no power flows through the sensor
-  digitalWrite(sensorPower, LOW);
+    // Set to LOW so no power flows through the sensor
+    digitalWrite(sensorPower, LOW);
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  //get the reading from the function below and print it
-  int level = readSensor();
+    //get the reading from the function below and print it
+    int level = readSensor();
 
-  Serial.print("Water level: ");
-  Serial.println(level);
+    Serial.print("Water level: ");
+    Serial.println(level);
 
-  delay(1000);
+    delay(1000);
 }
 
 //This is a function used to get the reading
 int readSensor() {
-  digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
-  delay(10);                        // wait 10 milliseconds
-  val = analogRead(sensorPin);      // Read the analog value form sensor
-  digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
-  return val;                       // send current reading
+    digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
+    delay(10);                        // wait 10 milliseconds
+    val = analogRead(sensorPin);      // Read the analog value form sensor
+    digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+    return val;                       // send current reading
 }
