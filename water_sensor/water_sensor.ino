11ce095e4ca8264948544eb339e51cf4b866fd39
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-10-29 09:25:12 -0700

    Final update

--- water_sensor/water_sensor.ino
+++ water_sensor/water_sensor.ino
@@ -2,27 +2,22 @@
 #define sensorPower 7
 #define sensorPin A0
 #define greenLED 2
-#define yellowLED 3
 #define redLED 4
-//#define H HIGH
-//#define L LOW
+#define H HIGH
+#define L LOW
 
 // Value for storing water level
 int val = 0;
-//const int greenLED = 2;
-//const int yellowLED = 3;
-//const int redLED 4;
 
 void setup() {
     // Set D7 as an OUTPUT
     pinMode(sensorPower, OUTPUT);
 
     pinMode(greenLED, OUTPUT);
-    pinMode(yellowLED, OUTPUT);
     pinMode(redLED, OUTPUT);
 
     // Set to LOW so no power flows through the sensor
-    digitalWrite(sensorPower, LOW);
+    digitalWrite(sensorPower, L);
     
     Serial.begin(9600);
 }
@@ -34,27 +29,21 @@ void loop() {
     Serial.print("Water level: ");
     Serial.println(level);
 
-    if (level < 48) {
-        digitalWrite(greenLED, HIGH);
-        digitalWrite(yellowLED, LOW);
-        digitalWrite(redLED, LOW);
-    } else if (level < 96) {
-        digitalWrite(greenLED, LOW);
-        digitalWrite(yellowLED, HIGH);
-        digitalWrite(redLED, LOW);
+    if (level < 170) {
+        digitalWrite(greenLED, H);
+        digitalWrite(redLED, L);
     } else {
-        digitalWrite(greenLED, LOW);
-        digitalWrite(yellowLED, LOW);
-        digitalWrite(redLED, HIGH);
+        digitalWrite(greenLED, L);
+        digitalWrite(redLED, H);
     }
-    delay(1000);
+    delay(500);
 }
 
 //This is a function used to get the reading
 int readSensor() {
-    digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
+    digitalWrite(sensorPower, H);  // Turn the sensor ON
     delay(10);                        // wait 10 milliseconds
     val = analogRead(sensorPin);      // Read the analog value form sensor
-    digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+    digitalWrite(sensorPower, L);   // Turn the sensor OFF
     return val;                       // send current reading
 }
